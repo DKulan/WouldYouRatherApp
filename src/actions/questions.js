f5da2911a04c6ca68dbfee1a0e6e90d0805d23d7
Daniel Kulangiev <danny.kulangiev@gmail.com>
2019-06-02 18:03:35 -0400

    Add ability to save a new question and save it to the external API

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,7 +1,7 @@
-import {_getQuestions, _saveQuestionAnswer} from '../API/_DATA'
+import {_getQuestions, _saveQuestionAnswer, _saveQuestion} from '../API/_DATA'
 import {ADD_VOTE, RECEIVE_QUESTIONS} from './constants'
 import {hideLoading, showLoading} from 'react-redux-loading-bar'
-import {addAnswer} from './users'
+import {addAnswer, getUserData} from './users'
 
 
 const storeQuestionData = (questions) => ({
@@ -28,11 +28,22 @@ const getQuestionData = () => {
   }
 }
 
+const saveQuestion = (question) => {
+  return (dispatch) => {
+    return _saveQuestion(question)
+      .then(() => {
+        dispatch(getQuestionData())
+        dispatch(getUserData())
+      })
+  }
+}
+
 const saveUserAnswer = (answerObj) => {
   return (dispatch) => {
     dispatch(addAnswer({...answerObj}))
     dispatch(addVote({...answerObj}))
 
+    // If there is an Error with the API we need to remove answer/vote (optimistic update)
     return _saveQuestionAnswer({...answerObj})
       .catch(() => {
         alert('Error saving user answer')
@@ -40,4 +51,4 @@ const saveUserAnswer = (answerObj) => {
   }
 }
 
-export {getQuestionData, saveUserAnswer}
+export {getQuestionData, saveUserAnswer, saveQuestion}
--- src/components/AddNewQuestion.js
+++ src/components/AddNewQuestion.js
@@ -1,38 +1,71 @@
 import React from 'react'
 import NavBar from './NavBar'
+import {connect} from 'react-redux'
+import {saveQuestion} from '../actions/questions'
 
 
-const AddNewQuestion = (props) => {
+class AddNewQuestion extends React.Component {
+  handleSubmit = () => {
+    this.props.dispatch(saveQuestion({
+      author: this.props.authedUser.id,
+      optionOneText: this.optionOneText.value,
+      optionTwoText: this.optionTwoText.value
+    }))
 
-  return (
-    <div>
-      <NavBar/>
-      <div className="hero">
-        <div className="hero-body">
-          <div className="panel">
-            <div className="panel-heading">
-              <h1 className="title has-text-primary">Create a new question</h1>
-            </div>
-            <div className="panel-block has-background-white-bis">
-              <div className="field">
-                <label className="label">Would you rather</label>
-                <div className="control">
-                  <input className="input is-half" type="text"/>
-                </div>
-                <label className="label">or</label>
-                <div className="control">
-                  <input className="input is-half" type="text"/>
-                </div>
-                <div className="control">
-                  <button className="button is-primary margin-top">Add</button>
+    this.props.history.push('/')
+  }
+
+  render() {
+    return (
+      <div>
+        <NavBar/>
+        <div className="hero">
+          <div className="hero-body">
+            <div className="panel">
+              <div className="panel-heading">
+                <h1 className="title has-text-primary">Create a new question</h1>
+              </div>
+              <div className="panel-block has-background-white-bis">
+                <div className="field">
+                  <label className="label">Would you rather</label>
+                  <div className="control">
+                    <input
+                      className="input is-half"
+                      type="text"
+                      ref={optionOneText => this.optionOneText = optionOneText}
+                    />
+                  </div>
+                  <label className="label">or</label>
+                  <div className="control">
+                    <input
+                      className="input is-half"
+                      type="text"
+                      ref={optionTwoText => this.optionTwoText = optionTwoText}
+                    />
+                  </div>
+                  <div className="control">
+                    <button
+                      onClick={this.handleSubmit}
+                      className="button is-primary margin-top"
+                    >Add
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default AddNewQuestion
+/*
+  p.s don't forget optimistic updates in the async action creators.
+ */
+
+const mapStateToProps = ({authedUser}) => ({
+  authedUser
+})
+
+export default connect(mapStateToProps)(AddNewQuestion)
